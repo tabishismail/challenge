bc45bf8fb09acbd591b51c71a4a786595a00e469
tabishismail <tabish554@gmail.com>
2021-05-19 22:46:03 +0500

    challenge # 2 done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
+// Challenge # 1
 
-var user = +prompt("Enter Intial Number");
-for (i = user; i >= 1; i--) {
-    for(j=1 ; j<=i; j++){
-    document.write( "*");
+// var user = +prompt("Enter Intial Number");
+// for (i = user; i >= 1; i--) {
+//     for(j=1 ; j<=i; j++){
+//     document.write( "*");
     
-    }
-    document.write( "<br />");
-}
+//     }
+//     document.write( "<br />");
+// }
+
+// Challenge # 2
+
+// var user = +prompt("Enter Intial Number");
+// for (i = 1; i <= user; i++) {
+//     document.write( "****"+ "<br />");
+//     }
