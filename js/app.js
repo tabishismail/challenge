1e75e116cd6d90a7f5a5b3a52d7aca0539eb4eaf
tabishismail <tabish554@gmail.com>
2021-05-19 22:50:32 +0500

    Challenge # 4 done

--- js/app.js
+++ js/app.js
@@ -18,8 +18,19 @@
 
 // Challenge # 3
 
+// var user = +prompt("Enter Intial Number");
+// for (i = 1; i <= user; i++) {
+//     for(j=1 ; j<=i; j++){
+//     document.write( "*");
+    
+//     }
+//     document.write( "<br />");
+// }
+
+// Challenge # 4
+
 var user = +prompt("Enter Intial Number");
-for (i = 1; i <= user; i++) {
+for (i = user; i >= 1; i--) {
     for(j=1 ; j<=i; j++){
     document.write( "*");
     
