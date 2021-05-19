d0a9881ab12a7beb8043c888069f19b22d9c37c9
tabishismail <tabish554@gmail.com>
2021-05-19 22:47:50 +0500

    Challenge # 3 done

--- js/app.js
+++ js/app.js
@@ -15,3 +15,14 @@
 // for (i = 1; i <= user; i++) {
 //     document.write( "****"+ "<br />");
 //     }
+
+// Challenge # 3
+
+var user = +prompt("Enter Intial Number");
+for (i = 1; i <= user; i++) {
+    for(j=1 ; j<=i; j++){
+    document.write( "*");
+    
+    }
+    document.write( "<br />");
+}
